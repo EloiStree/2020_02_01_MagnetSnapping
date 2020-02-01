8dd3217a9b4a9f741e38ebce42c38645051fd92b
streeeloi <streeeloi@gmail.com>
2020-02-01 14:47:02 +0100

    Save: 02/01/2020 2:47 PM

--- Runtime/Script/ActivableMagnet.cs
+++ Runtime/Script/ActivableMagnet.cs
@@ -1,32 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ActivableMagnet : MonoBehaviour
 {
 
+    public static List<ActivableMagnet> m_activesMagnetInScene = new List<ActivableMagnet>();
     public Transform m_linkedTransform;
     public float m_radius=1;
     public LayerMask m_affectable = -1;
     public bool m_snapToParent;
+    public float m_minDistanceofOtherToSnap= 0.003f;
 
+
+    public void OnEnable()
+    {
+        m_activesMagnetInScene.Add(this);
+    }
+    public void OnDisable()
+    {
+        m_activesMagnetInScene.Remove(this);
+    }
 
     public void SnapToFixMagnetAroundWithDefaultParameters()
     {
-        List<MagnetFixAnchor> fixmags = MagnetAnchorUtility.CheckForFixMagnet(m_linkedTransform.position, m_radius, m_affectable);
-        if (fixmags.Count > 0)
-        {
-            Snap(fixmags);
-        }
+        SnapToFixMagnetAtRangeWithDefaultParameters(m_radius);
     }
     public void SnapToFixMagnetAtRangeWithDefaultParameters(float radius)
     {
         List<MagnetFixAnchor> fixmags = MagnetAnchorUtility.CheckForFixMagnet(m_linkedTransform.position, radius, m_affectable);
         if (fixmags.Count > 0)
         {
+            if(GetActiveMagnetAround(m_minDistanceofOtherToSnap) <1)
             Snap(fixmags);
+
         }
     }
+
+    private int GetActiveMagnetAround(float radius)
+    {
+        return MagnetAnchorUtility.CheckForActivableMagnet(m_linkedTransform.position, radius).Count ;
+    }
+
     public void Unparent() {
         transform.parent = null;
     }
@@ -44,13 +61,18 @@ public class ActivableMagnet : MonoBehaviour
     {
         m_linkedTransform = this.transform;
     }
+
+    public Vector3 GetPosition()
+    {
+        return m_linkedTransform.position;
+    }
 }
 
 public class MagnetAnchorUtility {
-    public static List<MagnetFixAnchor> CheckForFixMagnet(Vector3 where, float radius, LayerMask layer )
+    public static List<MagnetFixAnchor> CheckForFixMagnet(Vector3 where, float radius, LayerMask layer)
     {
         List<MagnetFixAnchor> anchors = new List<MagnetFixAnchor>();
-        Collider [] hits =Physics.OverlapSphere(where, radius, layer);
+        Collider[] hits = Physics.OverlapSphere(where, radius, layer);
         for (int i = 0; i < hits.Length; i++)
         {
             MagnetFixAnchor anc = hits[i].GetComponent<MagnetFixAnchor>();
@@ -58,6 +80,18 @@ public class MagnetAnchorUtility {
                 anchors.Add(anc);
 
         }
+        anchors = anchors.OrderBy(k => Vector3.Distance(k.GetPosition(), where)).ToList();
         return anchors;
+    }
+    public static List<ActivableMagnet> CheckForActivableMagnet(Vector3 where, float radius)
+    {
+        List<ActivableMagnet> anchors = ActivableMagnet.m_activesMagnetInScene;
+        anchors = anchors.Where(k=>Vector3.Distance(where, k.GetPosition())<radius).OrderBy(k => Vector3.Distance(k.GetPosition(), where)).ToList();
+        return anchors;
+    }
+
+    public static bool IsInLayerMask(int layer, LayerMask layermask)
+    {
+        return layermask == (layermask | (1 << layer));
     }
 }
--- Runtime/Script/MagnetFixAnchor.cs
+++ Runtime/Script/MagnetFixAnchor.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MagnetFixAnchor : MonoBehaviour
 {
-  
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
 }
